3b96928f76fb7bd79f6751f79950db1387c98724
Josh Komoroske <jdkomo.dev@gmail.com>
2012-02-14 14:46:21 -0500

    Added a view for serving static content

---
+++ src/web/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+
+
+def static(request,path):
+	path='./statics'+'/'+path
+	fd=open(path,'r')
+	data=fd.read()
+	fd.close()
+	return HttpResponse(data)
