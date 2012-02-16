302a4ca21212207e2e93c320695b01c9bebc5a35
Tyler Campbell <tylercampbell@rpi-wl-237.dynamic.rpi.edu>
2012-02-16 17:44:41 -0500

    uncommented admin lines

--- src/web/urls.py
+++ src/web/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 from web.views import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^/?$',static,{'path':'statics/html/main.html'}),
