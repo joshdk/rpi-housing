487864b6019612ab30a05f4d58708747889f507f
Josh Komoroske <jdkomo.dev@gmail.com>
2012-02-21 15:17:40 -0500

    Reorganized urls and added API url base chain
    - URLs that atart with /api/ now chain to their own urls.py

--- src/web/urls.py
+++ src/web/urls.py
@@ -1,16 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
 from web.views import *
 
-# Uncomment the next two lines to enable the admin:
+
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-	url(r'^session/?$',session),
-	url(r'^/?$',static,{'path':'statics/html/main.html'}),
-	url(r'^(.*[^/])$',static),
+	url(r'^admin/',    include(admin.site.urls)),
 
-
-	# Uncomment the next line to enable the admin:
-   url(r'^admin/', include(admin.site.urls)),
+	url(r'^api/',      include('web.api.urls')),
+	url(r'^/?$',       'web.views.static',{'path':'statics/html/main.html'}),
+	url(r'^(.*[^/])$', 'web.views.static'),
 )
