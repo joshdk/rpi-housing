4a6df2ef360a063bc6f3e9068b1e9b38a8613427
unknown <lbassb@Laptop-b1.(none)>
2012-04-05 15:08:56 -0400

    User Regerstration Looks a tad better

--- src/web/data/javascript/homepage.js
+++ src/web/data/javascript/homepage.js
@@ -1,10 +1,38 @@
 
 $().ready(function() {	
-	mbox( '#lotteryStatus', 'pages/popup_lotteryStatus.php', 'Lottery Status' );
+	mboxLottery( '#lotteryStatus', 'pages/popup_lotteryStatus.php', 'Lottery Status' );
 	mbox( '#roomInvites', 'pages/popup_roomInvites.php', 'View Queue' );
 	mbox( '#viewQueue', 'pages/popup_viewQueue.php', 'Room Invites' ); 
 	mbox( '#searchFriends', 'pages/popup_searchFriends.php', 'Search Friends' ); 
 });	
+
+//Opens jQuery UI Dialog Box For the Lottery Status
+function mboxLottery( trigger, source, title ) {
+			
+	$(trigger).click(
+		function (){		
+			//define config object
+			$.fx.speeds._default = 500;
+			var dialogOpts = {
+					title: title,
+					modal: true,
+					autoOpen: false,
+					show: "blind",
+					closeOnEscape: true,
+					draggable: false,
+					resizable: false,
+					height: 165,
+					width: 400,
+					open: function() {
+					//display correct dialog content
+					$("#dialog-message").load(source);}
+					};
+			$("#dialog-message").dialog(dialogOpts);    //end dialog
+			$("#dialog-message").dialog("open");
+			return false;
+		}
+	);				
+}
 
 //Opens jQuery UI Dialog Box
 function mbox( trigger, source, title ) {
